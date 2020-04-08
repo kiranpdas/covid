e5febfa91fd113bc1b9a3acad46b2d6cd7efc1bc
Prasannadas, Kiran <kiranprasannadas@quickenloans.com>
2020-04-08 19:56:51 -0400

    added current count and updated date

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import argparse, json
+import argparse
+import json
 from src import utils, covid_tracker as ct
 
 
@@ -17,7 +18,11 @@ def main(args):
     covid_json = utils.get_url_as_json(config['covid_data_url'])
     world_pop_df = utils.read_csv_as_df(config['world_pop_csv_filename'])
 
-    type_available = {'c': 'confirmed', 'd': 'deaths', 'r': 'recovered'}
+    type_available = {'c': 'confirmed',
+                      'd': 'deaths',
+                      'r': 'recovered'}
+    format_available = {'p': 'percentage_of_population',
+                        'a': 'actual_count'}
 
     exit_flag = False
     while(not exit_flag):
@@ -32,33 +37,59 @@ def main(args):
                 exit(1)
 
             # Fetch user inputs
-            country = str(input('Enter country: '))
+            country = str(input(
+                'Enter country: '
+            ))
             type_input = str(input(
-                            'Enter count type{0}: '.format(
-                                            type_available)))
+                'Enter count type{0}: '.format(type_available)
+            )).lower()
+            format_input = str(input(
+                'Enter count format{0}: '.format(format_available)
+            )).lower()
 
             # Print the available countries during wrong user input
             if country in covid_json.keys() and \
-                    type_input in type_available.keys():
+                    type_input in type_available.keys() and \
+                    format_input in format_available:
                 input_loop_flag = False
             else:
-                print("\n ------ Wrong input, countries available are:\n\n{}\n\n".format(
-                    [key for key in covid_json.keys()]))
+                print(
+                 "\n ------ Wrong input, countries available are:\n\n{0}\n\n"
+                 .format([key for key in covid_json.keys()])
+                )
                 try_count = try_count + 1
-        # ############################## End of input loop
+            # ############################## End of input loop
 
-        # Set count_type
+        # Set count_type & count format
         count_type = type_available[type_input]
+        count_format = format_available[format_input]
 
-        # Execute count percent
-        count_percent = ct.get_percent_count(country=country,
-                                             count_type=count_type,
-                                             covid_json=covid_json,
-                                             world_pop_df=world_pop_df)
+        # Fetch the count output
+        if count_format == 'percentage_of_population':
+            count_out = str(
+                ct.get_percent_count(country=country,
+                                     count_type=count_type,
+                                     covid_json=covid_json,
+                                     world_pop_df=world_pop_df)
+                ) + '% ' + 'of total population'
+        else:
+            count_out = format(
+                ct.get_current_count(country=country,
+                                     count_type=count_type,
+                                     covid_json=covid_json),
+                ',d')
 
-        print("{0} count percent of {1} is {2}".format(count_type.capitalize(),
-                                                       country,
-                                                       count_percent))
+        # Fetch the updated date of covid data
+        covid_date = ct.get_covid_date(country=country, covid_json=covid_json)
+
+        # Display the output
+        print("\n```````````````````````````````````````````````````````````")
+        print("{0}'s {1} count is {2} on {3}"
+              .format(country,
+                      count_type.capitalize(),
+                      count_out,
+                      covid_date.strftime('%A, %b %d %Y')))
+        print("\n``````````````````````````````````````````````````````````")
 
         # Continue?
         if str(input("Do you want to continue(Y/N): ")).upper() in ['N', 'NO']:
--- src/covid_tracker.py
+++ src/covid_tracker.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from datetime import datetime
 
 
 def get_percent_count(country, count_type, covid_json, world_pop_df):
@@ -17,3 +18,29 @@ def get_percent_count(country, count_type, covid_json, world_pop_df):
     percent_count = covid_value / population_value * 100
 
     return round(percent_count, 5)
+
+
+def get_current_count(country, count_type, covid_json):
+    """Returns the actual count of given country and type
+
+    :param country: country for which covid data is needed
+    :param count_type: type of covid count ['confirmed', 'deaths', 'recovered']
+    :param covid_json: covid data json
+    :return: covid count for the given country and type
+    """
+
+    covid_value = covid_json[country][-1][count_type]
+    return covid_value
+
+
+def get_covid_date(country, covid_json):
+    """Returns the latest date of covid data
+
+    :param country: country for which covid data is needed
+    :param covid_json: covid data json
+    :return: latest date of the covid data as date object
+    """
+
+    covid_date = datetime.strptime(
+        covid_json[country][-1]['date'], '%Y-%m-%d')
+    return covid_date
