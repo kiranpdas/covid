0b384f7b3f29d3ce9623c85c6607371215fac925
Kiran Prasannadas <kiranprasannadas@gmail.com>
2022-06-25 14:23:00 -0400

    Fix: execute script

--- bin/exe_covid.sh
+++ bin/exe_covid.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Executes the covid count retriever
+source venv/bin/activate
 export PYTHONPATH=$PYTHONPATH:$(pwd)
-config_file=resources/config.json
-python src/main.py -c $config_file
+config_file=kpra_covid/resources/config.json
+python kpra_covid/main.py -c $config_file
